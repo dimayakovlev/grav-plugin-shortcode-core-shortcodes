ff13da06e7ea1faf2e5b8081eb8422bc49adf6df
Дмитрий Яковлев <web-dimayakovlev@outlook.com>
2017-03-23 16:21:05 +0400

    CSV support
    Content can be CSV (DSV) format.

--- RandomShortcode.php
+++ RandomShortcode.php
@@ -8,13 +8,19 @@ class RandomShortcode extends Shortcode
     public function init()
     {
         $this->shortcode->getHandlers()->add('random', function(ShortcodeInterface $sc) {
-            $delimeter = $sc->getParameter('delimeter', '|');
-            $result = $sc->getContent();
-            if ($delimeter) {
-                $array = explode($delimeter, $sc->getContent());
-                $result = $array[mt_rand(0, count($array)-1)];
+            
+            $content = $sc->getContent();
+            
+            if ($content) {
+                $delimeter = $sc->getParameter('delimeter', ',');
+                $enclosure = $sc->getParameter('enclosure', '"');
+                $escape = $sc->getParameter('escape', "\\");
+                $array = str_getcsv($content, $delimeter, $enclosure, $escape);
+                $content = $array[mt_rand(0, count($array)-1)];
             }
-            return $result;
+            
+            return $content;
+
         });
     }
 }
